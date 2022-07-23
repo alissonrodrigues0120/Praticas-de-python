0517a02c16acfab5dfa397305f0c5a6b337db13e
Álisson Rodrigues Fernandes <97190682+alissonrodrigues0120@users.noreply.github.com>
2022-07-23 13:43:12 -0300

    Delete pratica14 directory

--- pratica14/pratica14.py
+++
@@ -1,9 +0,0 @@
-def print_lol(the_list):
-    for each_item in the_list:
-        if isinstance(each_item, list):
-            print_lol(each_item)
-
-        else:
-            print(each_item)
-
-"""olá"""
--- pratica14/teste.py
+++
@@ -1,4 +0,0 @@
-import pratica14
-
-caos = [4, 5,[6, 7]]
-pratica14.print_lol(caos)
