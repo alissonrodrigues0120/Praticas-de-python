2fe4acf3de5af7ae2b7c3d2617ccc9cb5a191f0d
Álisson Rodrigues Fernandes <97190682+alissonrodrigues0120@users.noreply.github.com>
2022-07-30 17:17:40 -0300

    Add files via upload

---
+++ pratica18.py
@@ -0,0 +1,17 @@
+common=[3,4, 5, 6, 7, 34, 30, 65]
+
+print(common[0:4])
+print(common[4:7])
+print(common[7:8])
+
+
+#repeti a sequência 5 vezes
+print(common*5)
+
+change=[['_']*5 for i in range(8) ]
+
+change[3][4]='olá'
+
+print('\n'[1:])
+
+print(change)
