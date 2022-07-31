b9f7b00407684d88a4eee6780cc6cca4be0664b8
Álisson Rodrigues Fernandes <97190682+alissonrodrigues0120@users.noreply.github.com>
2022-07-31 15:24:42 -0300

    Add files via upload

---
+++ pratica19.py
@@ -0,0 +1,13 @@
+suco=['goiaba', 'limão', 'açaí', 'kiwi', 'pêssego']
+
+print(sorted(suco))
+
+print(sorted(suco, reverse=True))
+
+print(sorted(suco, key=len))
+
+print(suco)
+
+suco.sort()
+
+print(suco)
